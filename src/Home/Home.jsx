1493757369011bc7a331c0c9bed607af38f59ddd
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-23 14:02:32 +0530

    Home.css and input added

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your name"
+      className='name-input' />
+    </div>
+  )
+}
+
+export default Home
